af633ac93cbdaa7de35cc974e63101b2094cc13f
eladgurgigaspaces <elad.gur@gigaspaces.com>
2018-07-24 18:05:12 +0300

    Add file upload to cloudinary

--- src/main/webapp/companiesSignIn/SignUp/companiesSignUp.js
+++ src/main/webapp/companiesSignIn/SignUp/companiesSignUp.js
@@ -1,3 +1,35 @@
+function setFileUploadHandler() {
+    var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/shareandearn/upload'
+    var CLOUDINARY_UPLOAD_PRESET = 'odpbbrff';
+    var fileUpload = document.getElementById('file-upload');
+
+    fileUpload.addEventListener('change', function function_name(event) {
+        var file = event.target.files[0];
+        var formData = new FormData();
+
+        formData.append('file', file);
+        formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+        axios({
+            url: CLOUDINARY_URL,
+            method: 'POST',
+            headers: {
+                'content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: formData
+        }).then(function (res) {
+            console.log(res);
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+    });
+}
+
+window.onload = function () {
+    setFileUploadHandler();
+}
+
 function goBack() {
     window.history.back()
 }
@@ -10,3 +42,4 @@ function validateRepeatPassword(input) {
         input.setCustomValidity('');
     }
 }
+
